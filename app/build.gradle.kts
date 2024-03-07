c8f0e0eb9027bdecf47872b87f007d71600418a1
Ahmed-Bakr611 <ahmedhomes611@gmail.com>
2024-03-07 13:44:53 +0200

    edit calender icon and theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,7 @@ dependencies {
     //retrofit dependencie
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.android.material:material:1.5.0")
+
 
 }
