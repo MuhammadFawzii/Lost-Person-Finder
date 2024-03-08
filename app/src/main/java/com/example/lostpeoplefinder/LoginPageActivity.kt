bc348fa98b0c46d06e520651669b1cec9c9756ef
Muhammad-Fawzi <mohammed@20191700566mohammed20191700566@cis.asu.edu.eg>
2024-03-08 23:57:39 +0200

    Fawzi pull

--- app/src/main/java/com/example/lostpeoplefinder/LoginPageActivity.kt
+++ app/src/main/java/com/example/lostpeoplefinder/LoginPageActivity.kt
@@ -1,11 +1,71 @@
 package com.example.lostpeoplefinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.text.TextUtils // For checking empty strings
+import android.text.Editable // Represents editable text
+import android.text.TextWatcher
+import android.widget.EditText
 class LoginPageActivity : AppCompatActivity() {
+    private lateinit var et_Email: EditText
+    private lateinit var et_Password: EditText
+    private lateinit var tv_forgetPassword: TextView
+    private lateinit var tv_register: TextView
+    private lateinit var btn_Login: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        et_Email = findViewById(R.id.et_email_login)
+        et_Password = findViewById(R.id.et_password_login)
+        tv_forgetPassword = findViewById(R.id.tv_forgetPassword_Login)
+        tv_register = findViewById(R.id.tv_register_login)
+        btn_Login=findViewById(R.id.btn_loginPage)
+
+        // Add a TextWatcher to each EditText to dynamically update the button's state
+        addTextWatchersToEditTexts()
+        // Add a listener for the checkbox
+        updateButtonState()
+
+        setListeners()
+
+    }
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            updateButtonState()
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    }
+    private fun addTextWatchersToEditTexts() {
+        et_Email.addTextChangedListener(textWatcher)
+        et_Password.addTextChangedListener(textWatcher)
+    }
+    private fun updateButtonState() {
+        val isEmailValid = !TextUtils.isEmpty(et_Email.text.toString())
+        val isPasswordValid = !TextUtils.isEmpty(et_Password.text.toString())
+        btn_Login.isEnabled =  isEmailValid && isPasswordValid
+    }
+    private fun setListeners() {
+        tv_forgetPassword.setOnClickListener {
+            // Handle click on "Forget Password" text
+            val intent = Intent(this, ForgetPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        tv_register.setOnClickListener {
+            // Handle click on "Register" text
+            val intent = Intent(this, RegisiterPageActivity::class.java)
+            startActivity(intent)
+        }
+        btn_Login.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/lostpeoplefinder/RegisiterPageActivity.kt
+++ app/src/main/java/com/example/lostpeoplefinder/RegisiterPageActivity.kt
@@ -1,11 +1,91 @@
 package com.example.lostpeoplefinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.text.TextUtils // For checking empty strings
+import android.text.Editable // Represents editable text
+import android.text.TextWatcher
+import android.widget.EditText
+
 
 class RegisiterPageActivity : AppCompatActivity() {
+    private lateinit var etFullName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPhone: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var checkBox: CheckBox
+    private lateinit var btnNextRegister: Button
+    private lateinit var termsLink:TextView
+    private lateinit var policyLink:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regisiter_page)
+
+        // Initialize views
+        initViews()
+        // Add a TextWatcher to each EditText to dynamically update the button's state
+        addTextWatchersToEditTexts()
+        // Add a listener for the checkbox
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            updateButtonState()
+        }
+        setListeners()
+
     }
+
+    private fun initViews() {
+        etFullName = findViewById(R.id.et_full_name_register)
+        etEmail = findViewById(R.id.et_email_register)
+        etPhone = findViewById(R.id.et_phone_register)
+        etPassword = findViewById(R.id.et_password_reg)
+        checkBox = findViewById(R.id.checkbox_privacy_terms_reg)
+        btnNextRegister = findViewById(R.id.bt_Next_Register)
+        termsLink = findViewById(R.id.terms_link_reg)
+        policyLink = findViewById(R.id.policy_link_reg)
+    }
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            updateButtonState()
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    }
+    private fun addTextWatchersToEditTexts() {
+        etFullName.addTextChangedListener(textWatcher)
+        etEmail.addTextChangedListener(textWatcher)
+        etPhone.addTextChangedListener(textWatcher)
+        etPassword.addTextChangedListener(textWatcher)
+    }
+
+    private fun updateButtonState() {
+        val isChecked = checkBox.isChecked
+        val isFullNameValid = !TextUtils.isEmpty(etFullName.text.toString())
+        val isEmailValid = !TextUtils.isEmpty(etEmail.text.toString())
+        val isPhoneValid = !TextUtils.isEmpty(etPhone.text.toString())
+        val isPasswordValid = !TextUtils.isEmpty(etPassword.text.toString())
+        btnNextRegister.isEnabled = isChecked && isFullNameValid && isEmailValid && isPhoneValid && isPasswordValid
+    }
+    private fun setListeners() {
+        termsLink.setOnClickListener {
+            val intent = Intent(this, PrivacePolicyActivity::class.java) // Use class reference
+            startActivity(intent)
+        }
+        policyLink.setOnClickListener {
+            val intent = Intent(this, PrivacePolicyActivity::class.java) // Use class reference
+            startActivity(intent)
+        }
+        btnNextRegister.setOnClickListener {
+            val intent = Intent(this, VerifyCodeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
 }
